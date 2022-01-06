8a8ea84782f81fff215ec1b3d856812831f46d4e
Naufal700 <80891929+Naufal700@users.noreply.github.com>
2022-01-06 19:40:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ footer.innerHTML = sender;
 footer.href = "https://www.instagram.com/adamukti/";
 
 document.querySelector(".tombol").addEventListener('click', function() {
-    Swal.fire("Hallo Sayangku", "Aku ada pertanyaan nih buat kamu?", "question").then(function() {
+    Swal.fire("Hallo Mpi", "Aku ada pertanyaan nih buat kamu?", "question").then(function() {
         Swal.fire("Jawab yang jujur ya!").then(function() {
             Swal.fire("Awas aja kalo boong!!", "", "error").then(function() {
 
